6468a6685f7c4e27966a1b9d188d2f79892c8b54
Wangstaboi <rwryanwang@gmail.com>
2024-10-30 18:47:18 -0700

    Initial Commit

---
+++ Reverse-RMP/src/main/java/com/reverseRMP/Reverse_RMP/ReverseRmpApplication.java
@@ -0,0 +1,13 @@
+package com.reverseRMP.Reverse_RMP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseRmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseRmpApplication.class, args);
+	}
+
+}
---
+++ Reverse-RMP/src/test/java/com/reverseRMP/Reverse_RMP/ReverseRmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reverseRMP.Reverse_RMP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseRmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
